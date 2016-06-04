91734af569d4e6ea6eb8665c174a63fa22059baa
Antoine Gallix <antoine.gallix@gmail.com>
2016-06-04 04:27:31 +0200

    save notes and scratch files

--- scratch.py
+++ scratch.py
@@ -1,10 +1,12 @@
-from mplot.primitives import PitchClass
-from mplot.primitives import Interval
+from mplot.primitives import IntervalSet, Interval, IntervalClassSet
 
-# for n in [13, 0, -5, 16, 20, 40]:
-#     i = Interval(n)
-#     print "{} : {} : {}".format(n, i.name(), i.name('long'))
-
-
-for i in [-10, -7, -5, -3, 0, 1, 4, 5, 7, 13]:
-    print "{} : {}".format(i, i % 5)
+ins = IntervalClassSet()
+ins.add(Interval(4))
+ins.add(Interval(-1))
+ins2 = IntervalClassSet()
+ins2.add(4)
+ins2.add(Interval(11))
+print "ins",
+print ins
+print "ins==ins2",
+print ins == ins2
